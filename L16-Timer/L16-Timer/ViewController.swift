f7f8d2baf8eb9c7d0d546bbe04d67e64ff4db8d6
JackDaw1 <iaroshenko.ga@gmail.com>
2022-10-26 23:38:40 +0700

    Creating timer app

---
+++ L16-Timer/L16-Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L16-Timer
+//
+//  Created by Galina Iaroshenko on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
